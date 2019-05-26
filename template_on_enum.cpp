36b64a6627e270f2ede4cfa5454accc0b5604da1
Vesper Zhao <vesper.zhao@getcruise.com>
2019-05-26 08:38:01 -0700

    more experiment on enumn

--- template_on_enum.cpp
+++ template_on_enum.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
-enum PolicyType {Default, Greedy, NotImplemented};
+enum class PolicyType {Default, Greedy, NotImplemented};
+
+template<PolicyType T> struct priority {
+    enum {val = 99};
+};
+
+template<> struct priority<PolicyType::Default> { 
+   enum {val = 0};
+};
+
 
 template<PolicyType  T>class Policy {
 public:
     Policy() = default;
    ~Policy() = default;
+   const int _priority = priority<T>::val;
+   int set(){
+     //std::cout <<"Policy: " <<static_cast<int>(T) <<" "<<_priority<<  std::endl;
+	return 0;
+   }
 };
  
-template<>class Policy<Greedy> {
+template<>class Policy<PolicyType::Greedy> {
     int value;
     int method;
     int goal; 
@@ -15,30 +29,37 @@ public:
     Policy(int _value, int _method, int _goal):value(_value),method(_method),goal(_goal){}
    ~Policy() {}
     int set() {
-	std::cout <<"Policy: " << Greedy <<" "<< value << " " << method << " " << goal<< std::endl;
+	//std::cout <<"Policy: " << PolicyType::Greedy <<" "<< value << " " << method << " " << goal<< std::endl;
 	return 0;
     }
 }; 
 
-template<>class Policy<Default> {
+template<>class Policy<PolicyType::Default> {
    int value;
    int goal;
-   
+   const int _priority = priority<PolicyType::Default>::val;
 public:
    Policy(int _value, int _goal): value(_value), goal(_goal) {}
    ~Policy() {}
    int set() {
-     std::cout <<"Policy: " << Default <<" "<< value << " "  << goal<< std::endl;
+     //std::cout <<"Policy: " << PolicyType::Default <<" "<< value << " "  << goal<< std::endl;
+     std::cout<<"priority: " << _priority<<std::endl; 
      return 0;
    }
+   
 };
 
-
+struct Number_of_CPUs {
+   enum {val = 48;};
+};
 int main() {
-   Policy<Greedy> p{4,4,6};
+   Policy<PolicyType::Greedy> p{4,4,6};
    p.set();
-   Policy<Default> d{4,10};
+   Policy<PolicyType::Default> d{4,10};
    d.set();
-   Policy<NotImplemented> n;
+   Policy<PolicyType::NotImplemented> n;
    n.set();
+
+   PolicyType type = static_cast<PolicyType>(22);
+return 0;
 }
