f6ac063727e10338bd01a8036ed9fbf979ae45b7
Vesper Zhao <vesper.zhao@getcruise.com>
2019-09-12 09:07:17 -0700

    make global variable template work inside class template

--- template_on_string.cpp
+++ template_on_string.cpp
@@ -19,20 +19,30 @@ struct RecordTraits<RecordItem::thermals>{
     static constexpr const char *type="/thermals";
 };
 
+
+template <typename RecordTraits>
+std::string gRecordCollector = std::string(RecordTraits::type);
+
 template<typename RecordTraits>
-struct Record
+struct Recorder
 {
   void record(){
       std::string prefix = RecordTraits::type;
-      std::cout<<prefix<<std::endl;
+      std::cout<<"prefix: "<<prefix<<std::endl;
+      std::cout<<"gRecordCollector: "<<gRecordCollector<RecordTraits><<std::endl;
+      gRecordCollector<RecordTraits> = std::string_view(std::string("/secondary") + std::string(RecordTraits::type));
+      std::cout<<"gRecordCollector: "<<gRecordCollector<RecordTraits><<std::endl;
   }
 };
 
+
 int main() {
-    Record<RecordTraits<RecordItem::metrics>> metrics_rd;
-    Record<RecordTraits<RecordItem::thermals>> thermal_rd;
+    Recorder<RecordTraits<RecordItem::metrics>> metrics_rd;
+    Recorder<RecordTraits<RecordItem::thermals>> thermal_rd;
     metrics_rd.record();
     thermal_rd.record();
+    std::cout<<gRecordCollector<RecordTraits<RecordItem::metrics>><<std::endl;
+    std::cout<<gRecordCollector<RecordTraits<RecordItem::thermals>><<std::endl;
 }
 
 
