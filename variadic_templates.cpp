ef837a5840bceeb07130411d53b90397733575ec
Vesper Zhao <vesper.zhao@getcruise.com>
2020-01-09 09:56:35 -0800

    variadic template class on multiple parameter types

--- variadic_templates.cpp
+++ variadic_templates.cpp
@@ -8,27 +8,33 @@
 // So need to make sure the special cases are all defined before
 #include <iostream>
 #include <tuple>
-#include <utility>
 #include <type_traits>
+#include <utility>
 
-template <typename T> void P(T x) { std::cout << x << ' '; }
+template <typename T>
+void P(T x) {
+  std::cout << x << ' ';
+}
 
 void foo(char a) {
   P(3);
   P(a);
 }
 
-template <typename... Args> void foo(int a, Args... args) {
+template <typename... Args>
+void foo(int a, Args... args) {
   foo(args...);
   P(a);
 }
 
-template <typename... Args> void foo(char a, Args... args) {
+template <typename... Args>
+void foo(char a, Args... args) {
   P(a);
   foo(args...);
 }
 
-template <typename T, typename... Ts> struct ManyTypeMembers {
+template <typename T, typename... Ts>
+struct ManyTypeMembers {
   T primary;
   std::tuple<Ts...> secondary;
 
@@ -47,14 +53,52 @@ auto middle_man(int i, const Tuple &tpl, std::index_sequence<Is...>) {
   if constexpr (sizeof...(Is) > 0)
     return extra_stuff(i, std::get<Is>(tpl)...);
   else
-      std::cout<<"no extra stuff"<<std::endl;
-
+    std::cout << "no extra stuff" << std::endl;
 }
 
-template <typename Tuple> auto fun(const int &i, const Tuple &tpl) {
+template <typename Tuple>
+auto fun(const int &i, const Tuple &tpl) {
   return middle_man(i, tpl,
                     std::make_index_sequence<std::tuple_size_v<std::decay_t<decltype(tpl)>>>{});
 }
+
+enum COLOR { RED, MULTI };
+
+struct Red {
+  int width = 10;
+};
+
+struct Blue {
+  int width = 11;
+};
+
+struct Yellow {
+  int width = 12;
+};
+
+template <COLOR color, typename... ParamType>
+class Draw {
+ public:
+  Draw() = default;
+  Draw(ParamType... params) : _params{params...} {}
+  virtual void draw() = 0;
+
+ protected:
+  std::tuple<ParamType...> _params;
+};
+
+class DrawRed : public Draw<RED, Red> {
+ public:
+  void draw() { std::cout << "drawing red..." << std::get<0>(_params).width << std::endl; }
+};
+
+class DrawMulti : public Draw<MULTI, Blue, Yellow> {
+ public:
+  void draw() {
+    std::cout << "drawing blue..." << std::get<0>(_params).width << std::endl;
+    std::cout << "drawing yellow..." << std::get<1>(_params).width << std::endl;
+  }
+};
 
 int main() {
   foo('1', '2', 48, '4', '5');
@@ -72,5 +116,10 @@ int main() {
 
   auto result = fun(4, t1.secondary);
   fun(4, t2.secondary);
+
+  auto draw_multi = DrawMulti();
+  draw_multi.draw();
+  auto draw_red = DrawRed();
+  draw_red.draw();
   return 0;
 }
